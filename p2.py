5d72db2473001186f3273af1ba7348a13c650b03
iagooteroc <i.otero.coto@gmail.com>
2019-10-25 16:51:10 +0200

    Tiempos y estilo

--- p2.py
+++ p2.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 from pyspark import SparkContext, SparkConf
 from Bio import pairwise2
+import time
 # Para correr:
 # spark-submit p2.py 2>&1 | grep -v INFO
-
-conf = SparkConf().setAppName("ej1").setMaster("local[4]")
+n_proc = "local[2]"
+conf = SparkConf().setAppName("p2").setMaster(n_proc)
 sc = SparkContext(conf=conf)
 
 working_dir = 'material_spark/'
@@ -18,12 +19,23 @@ cadena_f = open(cadena_dir, "r")
 cadena = cadena_f.read()
 cadena_f.close()
 
-# rddAlineamientos = rddCadenas.map(lambda c: fitness_align(cadena,c))
-rddAlineamientos = rddCadenas.map(lambda c: pairwise2.align.globalms(cadena, c, 1, -1, -1, -1, penalize_end_gaps=(True, False))[0])
-# print(rddAlineamientos.first())
+start_time = time.time()
+rddAlineamientos = rddCadenas.map(lambda c: pairwise2.align.globalms(cadena, c, 1, -1, -1, -1, penalize_end_gaps=(True, False))[0]).map(lambda x: (x[2],x[0],x[1]))
+best_al = rddAlineamientos.max(lambda x: x[0])
+end_time1 = time.time()
+worst_al = rddAlineamientos.max(lambda x: -x[0])
+end_time2 = time.time()
+print('###################################')
+print('n_proc:',n_proc)
+print('===================================')
 print('Mayor puntuación:')
-print(rddAlineamientos.max(lambda x: x[2]))
+print(best_al)
+print('===================================')
+print('Time:', end_time1-start_time)
 print('===================================')
 print('Menor puntuación:')
-print(rddAlineamientos.max(lambda x: -x[2]))
+print(worst_al)
+print('===================================')
+print('Time:', end_time2-start_time)
+print('###################################')
 
